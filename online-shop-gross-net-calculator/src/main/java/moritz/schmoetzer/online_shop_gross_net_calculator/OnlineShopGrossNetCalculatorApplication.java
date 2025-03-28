6a3367490d5cc629d7180356008216357cc553b9
Moritz Schmötzer <schmoetzer.moritz@gmail.com>
2025-03-28 13:11:37 +0100

    created project

---
+++ online-shop-gross-net-calculator/src/main/java/moritz/schmoetzer/online_shop_gross_net_calculator/OnlineShopGrossNetCalculatorApplication.java
@@ -0,0 +1,13 @@
+package moritz.schmoetzer.online_shop_gross_net_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopGrossNetCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopGrossNetCalculatorApplication.class, args);
+	}
+
+}
---
+++ online-shop-gross-net-calculator/src/test/java/moritz/schmoetzer/online_shop_gross_net_calculator/OnlineShopGrossNetCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package moritz.schmoetzer.online_shop_gross_net_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopGrossNetCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
